dff3e1b72f7a078d59ec55a41cffaf83903d074e
Abdallah Hussein <abiza2013@gmail.com>
2023-12-30 00:20:40 +0200

    deleting redundant files

--- Services/MailGenerator.java
+++
@@ -1,9 +0,0 @@
-package Services;
-
-import Models.Customer;
-import Models.Order;
-
-public interface MailGenerator {
-//    mail --> order + fees + customer
-    String generator(Customer customer, Order order);
-}
